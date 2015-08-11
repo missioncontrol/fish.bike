c6a3bdbf88c22b13489f0190d4bacbaacc013a25
Ari Weinstein <arixmail@gmail.com>
2015-08-11 03:47:14 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>fish.bike</title>
+</head>
+<body>
+<marquee behavior="scroll" direction="left" scrollamount="30" truespeed="yes">
+<img src="FishBike.png" />
+</marquee>
+</body>
+</html>
